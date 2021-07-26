4168fd21924e4a986d81129a0350fbdbc4301b0a
saezurucrow <usobukuhito@gmail.com>
2021-07-26 14:47:01 +0900

    crosエラー解消

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'saezurucrow.github.io/dobble-site/'
+    origins 'saezurucrow.github.io'
 
     resource '*',
       headers: :any,
